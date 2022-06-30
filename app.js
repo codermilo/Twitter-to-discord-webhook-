d1456ab82bfff0dabb0fa760005558c6f2221b0a
dm16g12 <dan_friend@hotmail.co.uk>
2022-06-30 18:25:52 +0100

    added chalk

--- app.js
+++ app.js
@@ -19,9 +19,32 @@ const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
 // Beginning of Twitter API stream stuff
 const rules = [{ value: "from:Coach_Kenshin" }];
 
-//loglevel-prefix setup
+//loglevel-prefix and chalk setup
 prefix.reg(log);
 log.enableAll();
+
+const colors = {
+    TRACE: chalk.magenta,
+    DEBUG: chalk.cyan,
+    INFO: chalk.blue,
+    WARN: chalk.yellow,
+    ERROR: chalk.red,
+};
+
+prefix.reg(log);
+log.enableAll();
+
+prefix.apply(log, {
+    format(level, name, timestamp) {
+        return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)} ${chalk.green(`${name}:`)}`;
+    },
+});
+
+prefix.apply(log.getLogger('critical'), {
+    format(level, name, timestamp) {
+        return chalk.red.bold(`[${timestamp}] ${level} ${name}:`);
+    },
+});
 
 // Get stream rules 
 async function getRules() {
